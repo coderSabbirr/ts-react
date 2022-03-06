cd898503af4de2aaf1cbf1be1555a88a452ebcdb
coderSabbirr <sabirnahid5817@gmail.com>
2022-03-06 11:32:48 +0600

    all uplaod

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useReducer, useRef } from "react";
+import "./App.css";
+
+interface Todo {
+  id: number;
+  text: string;
+}
+type ActionType =
+  | { type: "ADD"; Text: "string" | any }
+  | { type: "REMOVE"; id: number };
 
 function App() {
+  function reducer(state: Todo[], action: ActionType) {
+    switch (action.type) {
+      case "ADD":
+        return [
+          ...state,
+          {
+            id: state.length,
+            text: action.Text,
+          },
+        ];
+      case "REMOVE":
+        return state.filter(({ id }) => id !== action.id);
+    }
+  }
+  const [todos, dispatch] = useReducer(reducer, []);
+  const newTodoRef = useRef<HTMLInputElement>(null);
+
+  const onAddTodo = useCallback(() => {
+    if (newTodoRef.current) {
+      dispatch({
+        type: "ADD",
+        Text: newTodoRef.current.value,
+      });
+      newTodoRef.current.value = "";
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: "center", marginTop: "10px" }}>
+      <input type="text" ref={newTodoRef} />
+      <button onClick={onAddTodo}>Add</button>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            {todo.text}
+
+            <button onClick={() => dispatch({ type: "REMOVE", id: todo.id })}>
+              Remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
